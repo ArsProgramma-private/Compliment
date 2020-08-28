bc5c21706a5ef49266aef441f3cb9792510aaea4
Malte Perenz <m.perenz@gmail.com>
2020-08-28 22:32:45 +0200

    Fixed initial config load

--- src/extension.ts
+++ src/extension.ts
@@ -6,6 +6,7 @@ import { messages } from './messages';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext): void {
+	reloadConfig(context);
 	vscode.workspace.onDidChangeConfiguration(config => reloadConfig(context));	
 	context.subscriptions.push(vscode.workspace.onWillSaveTextDocument(_=>showMessage((context.workspaceState.get('hideAfterMs') as any).duration)));
 }
@@ -17,7 +18,7 @@ const reloadConfig = (context: vscode.ExtensionContext): void => {
 
 function showMessage(dur: number): void {
 	const message: string = messages[Math.floor((Math.random()*messages.length))];
-	vscode.window.setStatusBarMessage(message, Math.abs(/* +currentConfig.get('hideAfterMs', 3000) */dur)  || 3000);
+	vscode.window.setStatusBarMessage(message, Math.abs(dur)  || 3000);
 }
 
 // this method is called when your extension is deactivated
